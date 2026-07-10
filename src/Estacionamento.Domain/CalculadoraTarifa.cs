dae7c6f612bd89016e719d78470cf401d5270b1a
Lucas Perez <lucas.perez@senior.com.br>
2026-07-10 15:36:19 -0300

    feat: implementa calculo de horas adicionais com tolerancia de 10 minutos por hora

--- src/Estacionamento.Domain/CalculadoraTarifa.cs
+++ src/Estacionamento.Domain/CalculadoraTarifa.cs
@@ -4,18 +4,27 @@ namespace Estacionamento.Domain;
 
 public class CalculadoraTarifa
 {
+
+    private static readonly TimeSpan TempoLimiteMeiaHora = TimeSpan.FromMinutes(30);
+    private static readonly TimeSpan DuracaoHora = TimeSpan.FromHours(1);
+    private static readonly TimeSpan ToleranciaAdicional = TimeSpan.FromMinutes(10);
+
+
     public decimal Calcular(TimeSpan tempoPermanencia, TabelaPreco tabelaPreco)
     {
-        if(tempoPermanencia <= TimeSpan.FromMinutes(30))
+        if(tempoPermanencia <= TempoLimiteMeiaHora)
         {
             return tabelaPreco.ValorHoraInicial / 2;
         }
 
-         if(tempoPermanencia <= TimeSpan.FromHours(1))
+         if(tempoPermanencia <= DuracaoHora + ToleranciaAdicional)
         {
             return tabelaPreco.ValorHoraInicial;
         }
 
-        throw new NotImplementedException();
+        var tempoAlemDaPrimeiraHora = tempoPermanencia - DuracaoHora;
+        var horasAdicionais = Math.Ceiling((tempoAlemDaPrimeiraHora - ToleranciaAdicional).TotalMinutes / DuracaoHora.TotalMinutes);
+
+        return tabelaPreco.ValorHoraInicial + (decimal)horasAdicionais * tabelaPreco.ValorHoraAdicional;
     }
 }
